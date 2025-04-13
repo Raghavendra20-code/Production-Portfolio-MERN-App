7ec27977870ebb0d27f16880916467c78dd8dd21
Raghavendra20-code <126390379+Raghavendra20-code@users.noreply.github.com>
2025-04-13 12:38:19 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, './client/build')))
 
 app.use('/api/v1/portfolio/', portfolioRoutes)
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
 })
 
