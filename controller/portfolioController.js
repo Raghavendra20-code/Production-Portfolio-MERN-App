8e9f59597c1d64be26687227f60120d9936f1d1e
Raghavendra20-code <126390379+Raghavendra20-code@users.noreply.github.com>
2025-04-13 11:48:06 +0530

    Add files via upload

--- controller/portfolioController.js
+++ controller/portfolioController.js
@@ -7,24 +7,39 @@ const sendEmailController = async (req, res) => {
     try {
         const { name, email, message } = req.body;
 
+        const recipeintMessage = `Hello ${name},\nThank you so much for taking the time to explore my portfolio application. I really appreciate your interest, and I’m glad you had the chance to go through it.
+
+        I’ll be reviewing your message and will get back to you shortly with more details. In the meantime, please don’t hesitate to reach out if you have any questions or need anything else from my side.
+        
+        Looking forward to connecting again soon! \n\nBest regards,\nRaghavendra Manne\n7410150656`
+
         // Set up transporter (example using Gmail SMTP)
         const transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
-                user: 'raghav.manne20@gmail.com',
-                pass: 'iqwd ublb stzc sffq' // Use an app password or environment variable
+                user: process.env.EMAIL,
+                pass: process.env.APP_PASSWORD // Use an app password or environment variable
             }
         });
 
         // Email options
         const mailOptions = {
-            from: 'raghav.manne20@gmail.com',
+            from: process.env.EMAIL,
             to: email,
-            subject: `Mail from ${name}`,
-            text: message
+            subject: `Your mail recieved`,
+            text: recipeintMessage
         };
 
+        const myMailOptions = {
+            from: process.env.EMAIL,
+            to: process.env.EMAIL,
+            subject: `Mail from ${name}`,
+            text: `${message}\nMail ID: ${email}`
+        }
+
         const info = await transporter.sendMail(mailOptions);
+
+        await transporter.sendMail(myMailOptions);
 
         return res.status(200).send({
             success: true,
